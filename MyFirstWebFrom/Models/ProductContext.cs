76e882b4e7e9fa6b17454ffd943d92f3b669daf3
John Paul <john.garcia@winetsolutions.it>
2017-08-23 16:43:28 +0200

    Display Data Items and Detail

---
+++ MyFirstWebFrom/Models/ProductContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MyFirstWebForm.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("MyFirstWebForm") { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ MyFirstWebFrom/Site.Master.cs
@@ -0,0 +1,26 @@
+﻿using MyFirstWebForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyFirstWebForm
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public IQueryable<Category> GetCategories()
+        {
+            var _db = new MyFirstWebForm.Models.ProductContext();
+            IQueryable<Category> query = _db.Categories;
+            return query;
+        }
+
+    }
+}
